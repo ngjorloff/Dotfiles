fc33c17ff837c954ea2c5b5aa9fb3ec6db3770f4
ngjorloff <niklas.gjorloff@gmail.com>
2021-12-26 17:40:02 +0100

    Initial commit

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,4 @@
+Set-Alias v nvim.exe
+Set-Alias vg nvim-qt.exe
+
+oh-my-posh --init --shell pwsh --config C:\Users\nikla\AppData\Local\Programs\oh-my-posh\themes\dracula.omp.json | Invoke-Expression
