e7084083d2528dd669d76fcb6e362a1dd2da8ba2
ngjorloff <niklas.gjorloff@gmail.com>
2022-04-17 17:59:37 +0200

    Update PowerShell profile

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,9 @@
+# Neovim
+
 Set-Alias v nvim.exe
 Set-Alias vg nvim-qt.exe
+
+# Oh my Posh
 
 $ompPath = "$env:LOCALAPPDATA\Programs\oh-my-posh\themes"
 $ompTheme = "$ompPath\gruvboxlight.omp.json"
@@ -7,10 +11,13 @@ $ompTheme = "$ompPath\gruvboxlight.omp.json"
 oh-my-posh --init --shell pwsh --config "$ompTheme" | Invoke-Expression
 
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle ListView
 
 function repos {
     Set-Location "$env:USERPROFILE\source\repos"
 }
+
+# Configs
 
 function nvimconf {
     Set-Location "$env:LOCALAPPDATA\nvim"
@@ -18,4 +25,41 @@ function nvimconf {
 
 function psconf {
     nvim $PROFILE
+}
+
+# Git aliases
+
+function gst {
+    & git status
+}
+
+function gsts {
+    & git status -s
+}
+
+function gca {
+    & git commit -a
+}
+
+# Checkout branch
+
+function co {
+    param(
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [ArgumentCompleter({
+            param($pCmd, $pParam, $pWord, $pAst, $pFakes)
+
+            $branchList = (git branch --format='%(refname:short)')
+
+            if ([string]::IsNullOrWhiteSpace($pWord)) {
+                return $branchList;
+            }
+
+            $branchList | Select-String "$pWord"
+        })]
+        [string] $branch
+    )
+
+    git switch $branch;
 }
